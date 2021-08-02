6375b65e2547194e3d8d3106be088179589d78e2
anuj9604 <62599607+anuj9604@users.noreply.github.com>
2021-08-02 13:33:18 +0530

    Add files via upload

---
+++ numtowords.py
@@ -0,0 +1,6 @@
+def numtowords():
+    words=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+    inp=int(input("Enter a numerical value from 0-9: "))
+    print(words[inp])
+
+numtowords()
